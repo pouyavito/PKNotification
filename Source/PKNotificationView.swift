a2da4221657d65f5e9bfb2c040127abc8c00a8b9
Pouya Kahfi <pouya.kahfi@gmail.com>
2018-10-07 00:09:49 +0330

    added source folder added PKNotificationsView.swift file

---
+++ Source/PKNotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  PKNotificationView.swift
+//  PKNotification
+//
+//  Created by Pouya Kahfi on 10/7/18.
+//  Copyright © 2018 Pouya Kahfi. All rights reserved.
+//
+
+import UIKit
+
+class PKNotificationView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
